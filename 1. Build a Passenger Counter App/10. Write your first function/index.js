74fff8783c20eddd5f282f6e6ce4188bd9d57c1d
Patrick Aziken <onaziken@gmail.com>
2022-06-29 13:35:38 +0100

    Learning increments and functions

--- 1. Build a Passenger Counter App/10. Write your first function/index.js
+++ 1. Build a Passenger Counter App/10. Write your first function/index.js
@@ -1,3 +1,8 @@
 // Create a function (you decide the name) that logs out the number 42 to the console
 // Call/invoke the function
 
+function randomNumber() {
+    console.log(Math.round(Math.random()));
+}
+
+randomNumber();
--- 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -1,10 +1,19 @@
 // document.getElementById("count").innerText = 5
 
-let count = 5
+// let count = 5
 
-console.log(count)
+// console.log(count)
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
 
+let bonusPoints = 50
 
+bonusPoints = bonusPoints + 50
+console.log(`Bonus points: ${bonusPoints}`)
+
+bonusPoints = bonusPoints - 75
+console.log(`Bonus points: ${bonusPoints}`)
+
+bonusPoints = bonusPoints + 45
+console.log(`Bonus points: ${bonusPoints}`)
