d09618eb24167cd51e55e5d46eb65a88a9b5c74c
Patrick Aziken <1462747+skylarng89@users.noreply.github.com>
2022-08-07 18:07:38 +0100

    - Incrementing and Decrementing

--- 1. Build a Passenger Counter App/25. Create the save feature/index.js
+++ 1. Build a Passenger Counter App/25. Create the save feature/index.js
@@ -12,6 +12,6 @@ function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
     // 3. Render the variable in the saveEl using innerText
     // NB: Make sure to not delete the existing content of the paragraph
-    saveEl.innerText = count + " - ";
-    console.log(count);
+    let previousEntries = count + " - "
+    saveEl.innerText += ` ${previousEntries}`
 }
--- 1. Build a Passenger Counter App/8. The onclick event listener/index.js
+++ 1. Build a Passenger Counter App/8. The onclick event listener/index.js
@@ -8,7 +8,3 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
-
-
-
-
