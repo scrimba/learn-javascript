ca17ba761695c8564fc84752a3528d75a9463db9
Riuqlav <valquirsolza.vs@gmail.com>
2022-11-02 17:33:06 +0100

    .

--- 4. Practice time - part 2/6. Rock papers scissors/index.js
+++ 4. Practice time - part 2/6. Rock papers scissors/index.js
@@ -1,5 +1,9 @@
-let hands = ["rock", "paper", "scissor"]
+let hands = ["rock", "paper", "scissor"];
 
 // Create a function that returns a random item from the array
 
-
+const getRamdomItem = () => {
+  let ramdomItem = Math.floor(Math.random() * 3);
+  console.log(hands[ramdomItem]);
+};
+getRamdomItem();
