cd2a87da79db3184bbcb58094ff2504ba72a9c9b
Riuqlav <valquirsolza.vs@gmail.com>
2022-11-03 10:27:59 +0100

    addEventListener

--- 5. Build a Chrome Extension/6. Write your first addEventListener()/index.js
+++ 5. Build a Chrome Extension/6. Write your first addEventListener()/index.js
@@ -2,7 +2,8 @@
 boxEl = document.getElementById("box");
 // 2. Add a click event listener to the box
 //done
+
 // 3. Log out "I want to open the box!" when it's clicked
-const openBox = () => {
+boxEl.addEventListener("click", () => {
   console.log("the box is open!");
-};
+});
