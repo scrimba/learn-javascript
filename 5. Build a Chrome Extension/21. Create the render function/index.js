0b6947d4a770a163ba9541b9b38c2e12e166e705
Riuqlav <valquirsolza.vs@gmail.com>
2022-11-07 09:17:01 +0100

    .

--- 5. Build a Chrome Extension/21. Create the render function/index.js
+++ 5. Build a Chrome Extension/21. Create the render function/index.js
@@ -3,15 +3,20 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", function () {
+inputBtn.addEventListener("click", function (event) {
   myLeads.push(inputEl.value);
   console.log(myLeads);
-  // 2. Call the renderLeads() function
+  renderLeads();
+  event.preventDefault();
+
+  inputEl.value = "";
 });
 
 // 1. Wrap the code below in a renderLeads() function
-let listItems = "";
-for (let i = 0; i < myLeads.length; i++) {
-  listItems += "<li>" + myLeads[i] + "</li>";
-}
-ulEl.innerHTML = listItems;
+const renderLeads = () => {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+};
