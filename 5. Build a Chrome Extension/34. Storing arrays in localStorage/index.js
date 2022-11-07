7b574f678103cf52749da0a081db713679ae467d
Riuqlav <valquirsolza.vs@gmail.com>
2022-11-07 17:25:45 +0100

    .

--- 5. Build a Chrome Extension/34. Storing arrays in localStorage/index.js
+++ 5. Build a Chrome Extension/34. Storing arrays in localStorage/index.js
@@ -1,27 +1,33 @@
-let myLeads = ["www.awesomelead.com"]
+let myLeads = `["www.awesomelead.com"]`;
 
+myLeads = JSON.parse(myLeads);
 
+myLeads.push("dfsdfsdfsdf");
 
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+myLeads = JSON.stringify(myLeads);
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+console.log(typeof myLeads);
+
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
 
 function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
