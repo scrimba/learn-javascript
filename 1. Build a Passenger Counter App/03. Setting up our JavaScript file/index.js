25bccbfa18e52f0384c8277bed6ddab73803a742
Septimiu-Iulian Calin <septimiu.calin@gocity.com>
2023-09-08 11:43:11 +0100

    added increment and save buttons (up to lesson 17)

---
+++ 1. Build a Passenger Counter App/03. Setting up our JavaScript file/index.js
@@ -0,0 +1,11 @@
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+function increment() {
+    count += 1;
+    countEl.innerText = count;
+}
+
+function save() {
+    console.log(count);
+}
